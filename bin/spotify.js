3e553e91000b5d23e17fffaf129b016efe5ff8f5
John Katsnelson <johnkatsnelson@gmail.com>
2016-03-05 14:10:43 -0800

    adds an artist search library

--- bin/spotify.js
+++ bin/spotify.js
@@ -5,23 +5,44 @@
  */
 
 const program = require('commander');
-const child_process = require('child_process');
+const spotify = require('spotify-node-applescript');
+const SpotifyWebApi = require('spotify-web-api-node');
 const _ = require('lodash');
 
-const shellWrap = function (cmd) {
-  return `(echo 'window = this;'; cat spotify-application/${cmd}.js; echo ';ObjC.import(\"stdlib\");$.exit(0)') | osascript -l JavaScript`
-};
-
-const appleCommand = function (cmd) {
-  program.command(cmd)
-          .action(function() {
-            child_process.execSync(shellWrap(cmd))
-          });
-};
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+  clientId : 'a69ee9ea93c8453899c7a0c062711f5c',
+  clientSecret : 'a29842714cb9420b99da3505ca5069af'
+});
 
 program
   .version('0.0.1');
 
-_.each(['pause', 'play'], appleCommand)
+program.command('play')
+        .action(function() {
+          spotify.play()
+        });
+
+program.command('search <type> <string>')
+        .action(function (type, string) {
+          if (type === 'artist') {
+            console.log('searching artist');
+            spotifyApi.searchArtists(string).then(function(output) {
+              _.each(output.body.artists.items, function(artist) {
+                console.log(artist.name);
+              });
+            });
+          }
+
+          if (type === 'track') {
+            console.log('searching tracks')
+          }
+
+          if (type === 'playlist') {
+            console.log('searching playlist')
+          }
+        });
+
 
 program.parse(process.argv);
+
